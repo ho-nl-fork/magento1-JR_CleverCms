acd10a625fa7bfeefcbe747045976dc0c9483c9d
Maikel Koek <maikel@h-o.nl>
2016-06-30 18:03:31 +0200

    [OPPO-1091] +: 'store_ids' column added to cms_page_tree table

---
+++ app/code/community/JR/CleverCms/sql/clever_cms_setup/upgrade-2.1.4-2.1.5.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * JR_CleverCms
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    JR
+ * @package     JR_CleverCms
+ * @copyright   Copyright © 2016 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @author      Maikel Koek – H&O <info@h-o.nl>
+ */
+
+/* @var $installer JR_CleverCms_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->getConnection()->addColumn(
+    $this->getTable('cms/page_tree'),
+    'store_ids',
+    'varchar(255) null default null'
+);
+
+$installer->endSetup();
