8303290d890c73890e96efb58fa941f894f56214
Maikel Koek <maikel@h-o.nl>
2016-08-11 16:39:46 +0200

    [OPPO-1091] +: Store ID selector and new input field names for design tab

--- app/code/community/JR/CleverCms/Block/Adminhtml/Cms/Page/Edit/Tab/Design.php
+++ app/code/community/JR/CleverCms/Block/Adminhtml/Cms/Page/Edit/Tab/Design.php
@@ -23,12 +23,22 @@
  * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
 class JR_CleverCms_Block_Adminhtml_Cms_Page_Edit_Tab_Design
     extends Mage_Adminhtml_Block_Cms_Page_Edit_Tab_Design
 {
+    /**
+     * @var Mage_Core_Model_Store
+     */
+    protected $_store = null;
 
     protected function _prepareForm()
     {
+        $helper = Mage::helper('jr_clevercms/cms_page');
+
+        /** @var $model Mage_Cms_Model_Page */
+        $model = Mage::registry('cms_page');
+
         /*
          * Checking if user have permissions to save information
          */
@@ -38,86 +48,110 @@ class JR_CleverCms_Block_Adminhtml_Cms_Page_Edit_Tab_Design
             $isElementDisabled = true;
         }
 
+        $store = $this->getStore();
+        $model = $helper->getPage($model, $store);
+
         $form = new Varien_Data_Form();
 
         $form->setHtmlIdPrefix('page_');
 
-        $model = Mage::registry('cms_page');
-
-        $layoutFieldset = $form->addFieldset('layout_fieldset', array(
-            'legend' => Mage::helper('cms')->__('Page Layout'),
+        $layoutFieldset = $form->addFieldset('layout_fieldset', [
+            'legend' => $helper->__('Page Layout'),
             'class'  => 'fieldset-wide',
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-        $layoutFieldset->addField('root_template', 'select', array(
-            'name'     => 'root_template',
-            'label'    => Mage::helper('cms')->__('Layouts'),
+        $useStoreId = $layoutFieldset->addField('page[design][' . $store->getId() . '][use_store_id]', 'select', [
+            'name'      => 'page[design][' . $store->getId() . '][use_store_id]',
+            'label'     => $helper->__('Use Store View'),
+            'title'     => $helper->__('Use Store View'),
+            'values'    => Mage::getSingleton('ho_cms/system_store')->getOptions($store->getId()),
+            'value'     => $helper->getUseStoreIdValue($model, $store),
+        ]);
+
+        $layoutFieldset->addField('page[design][' . $store->getId() . '][root_template]', 'select', [
+            'name'     => 'page[design][' . $store->getId() . '][root_template]',
+            'label'    => $helper->__('Layouts'),
             'values'   => Mage::getSingleton('catalog/category_attribute_source_layout')->getAllOptions(),
-            'disabled' => $isElementDisabled
-        ));
+            'disabled' => $isElementDisabled,
+        ]);
 
-        $layoutFieldset->addField('layout_update_xml', 'textarea', array(
-            'name'      => 'layout_update_xml',
-            'label'     => Mage::helper('cms')->__('Layout Update XML'),
+        $layoutFieldset->addField('page[design][' . $store->getId() . '][layout_update_xml]', 'textarea', [
+            'name'      => 'page[design][' . $store->getId() . '][layout_update_xml]',
+            'label'     => $helper->__('Layout Update XML'),
             'style'     => 'height:24em;',
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-        $designFieldset = $form->addFieldset('design_fieldset', array(
-            'legend' => Mage::helper('cms')->__('Custom Design'),
+        $designFieldset = $form->addFieldset('design_fieldset', [
+            'legend' => $helper->__('Custom Design'),
             'class'  => 'fieldset-wide',
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
         $dateFormatIso = Mage::app()->getLocale()->getDateFormat(
             Mage_Core_Model_Locale::FORMAT_TYPE_SHORT
         );
 
-        $designFieldset->addField('custom_theme_from', 'date', array(
-            'name'      => 'custom_theme_from',
-            'label'     => Mage::helper('cms')->__('Custom Design From'),
+        $designFieldset->addField('page[design][' . $store->getId() . '][custom_theme_from]', 'date', [
+            'name'      => 'page[design][' . $store->getId() . '][custom_theme_from]',
+            'label'     => $helper->__('Custom Design From'),
             'image'     => $this->getSkinUrl('images/grid-cal.gif'),
             'format'    => $dateFormatIso,
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-        $designFieldset->addField('custom_theme_to', 'date', array(
-            'name'      => 'custom_theme_to',
-            'label'     => Mage::helper('cms')->__('Custom Design To'),
+        $designFieldset->addField('page[design][' . $store->getId() . '][custom_theme_to]', 'date', [
+            'name'      => 'page[design][' . $store->getId() . '][custom_theme_to]',
+            'label'     => $helper->__('Custom Design To'),
             'image'     => $this->getSkinUrl('images/grid-cal.gif'),
             'format'    => $dateFormatIso,
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-        $designFieldset->addField('custom_theme', 'select', array(
-            'name'      => 'custom_theme',
-            'label'     => Mage::helper('cms')->__('Custom Theme'),
+        $designFieldset->addField('page[design][' . $store->getId() . '][custom_theme]', 'select', [
+            'name'      => 'page[design][' . $store->getId() . '][custom_theme]',
+            'label'     => $helper->__('Custom Theme'),
             'values'    => Mage::getModel('core/design_source_design')->getAllOptions(),
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-
-        $designFieldset->addField('custom_root_template', 'select', array(
-            'name'      => 'custom_root_template',
-            'label'     => Mage::helper('cms')->__('Custom Layout'),
+        $designFieldset->addField('page[design][' . $store->getId() . '][custom_root_template]', 'select', [
+            'name'      => 'page[design][' . $store->getId() . '][custom_root_template]',
+            'label'     => $helper->__('Custom Layout'),
             'values'    => Mage::getSingleton('catalog/category_attribute_source_layout')->getAllOptions(),
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-        $designFieldset->addField('custom_layout_update_xml', 'textarea', array(
-            'name'      => 'custom_layout_update_xml',
-            'label'     => Mage::helper('cms')->__('Custom Layout Update XML'),
+        $designFieldset->addField('page[design][' . $store->getId() . '][custom_layout_update_xml]', 'textarea', [
+            'name'      => 'page[design][' . $store->getId() . '][custom_layout_update_xml]',
+            'label'     => $helper->__('Custom Layout Update XML'),
             'style'     => 'height:24em;',
-            'disabled'  => $isElementDisabled
-        ));
+            'disabled'  => $isElementDisabled,
+        ]);
 
-        Mage::dispatchEvent('adminhtml_cms_page_edit_tab_design_prepare_form', array('form' => $form));
-
-        $form->setValues($model->getData());
+        Mage::dispatchEvent('adminhtml_cms_page_edit_tab_design_prepare_form', ['form' => $form]);
 
         $this->setForm($form);
 
+        return $this;
+    }
+
+    /**
+     * @return Mage_Core_Model_Store
+     */
+    public function getStore()
+    {
+        return $this->_store;
+    }
+
+    /**
+     * @param Mage_Core_Model_Store $store
+     * @return $this
+     */
+    public function setStore($store)
+    {
+        $this->_store = $store;
         return $this;
     }
 }
